42e332926ad041d21961e7d2bc786685d061b0a7
Jaime <sotojaimeantonio@gmail.com>
2020-08-17 17:13:52 -0400

    ultimo commit

--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -43,6 +43,40 @@
             </b-col>
         </b-row>
 
+        <b-row class="mt-5">
+            <!-- VAMOS A PONER LAS TARJETAS O CARDS https://bootstrap-vue.org/docs/components/card -->
+            <b-col cols="12" md="4">
+                <!-- SE PONE B-CARD PARA INDICAR QUE ES UNA TARJETA COME LA IMG DE LA PAG DE BOOTSTRAP..SOLO COPIE LO QUE ESTA AQUI DE LA PAG. DE BOOTSTRAP -->
+                <b-card-img :src="require('../assets/1.png')" img-top>     
+                </b-card-img>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione inventore velit ipsam dignissimos, quis facere ipsa minus accusantium nemo, sint illum unde corrupti! Laboriosam, repudiandae ex odit facilis possimus corporis.</p>
+                <!-- VOY A PONER UN BOTON -->
+                <b-button variant="danger" href="#">QUE LINDA FOTO</b-button>
+                <br><br>
+            </b-col>
+
+            <b-col cols="12" md="4">
+                <!-- SE PONE B-CARD PARA INDICAR QUE ES UNA TARJETA COME LA IMG DE LA PAG DE BOOTSTRAP..SOLO COPIE LO QUE ESTA AQUI DE LA PAG. DE BOOTSTRAP -->
+                <b-card-img :src="require('../assets/2.png')" img-top>     
+                </b-card-img>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione inventore velit ipsam dignissimos, quis facere ipsa minus accusantium nemo, sint illum unde corrupti! Laboriosam, repudiandae ex odit facilis possimus corporis.</p>
+                <!-- VOY A PONER UN BOTON -->
+                <b-button variant="danger" href="#">QUE LINDA FOTO</b-button>
+                <br><br>
+            </b-col>
+
+            <b-col cols="12" md="4">
+                <!-- SE PONE B-CARD PARA INDICAR QUE ES UNA TARJETA COME LA IMG DE LA PAG DE BOOTSTRAP..SOLO COPIE LO QUE ESTA AQUI DE LA PAG. DE BOOTSTRAP -->
+                <b-card-img :src="require('../assets/3.png')" img-top>     
+                </b-card-img>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione inventore velit ipsam dignissimos, quis facere ipsa minus accusantium nemo, sint illum unde corrupti! Laboriosam, repudiandae ex odit facilis possimus corporis.</p>
+                <!-- VOY A PONER UN BOTON -->
+                <b-button variant="danger" href="#">QUE LINDA FOTO</b-button>
+                <br><br>
+            </b-col>
+        </b-row>
+
+
 
 
 
