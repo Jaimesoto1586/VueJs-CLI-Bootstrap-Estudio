3c9f9f25377588c87b2023e23532bb5a31aa9dce
Jaime <sotojaimeantonio@gmail.com>
2020-08-17 16:55:22 -0400

    Fotos cambiadas

--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -14,35 +14,34 @@
             <!-- VAMOS A PONER LAS TARJETAS O CARDS https://bootstrap-vue.org/docs/components/card -->
             <b-col cols="12" md="4">
                 <!-- SE PONE B-CARD PARA INDICAR QUE ES UNA TARJETA COME LA IMG DE LA PAG DE BOOTSTRAP..SOLO COPIE LO QUE ESTA AQUI DE LA PAG. DE BOOTSTRAP -->
-                <b-card img-src="https://picsum.photos/600/300/?image=28" img-top
-                > 
+                <b-card-img :src="require('../assets/1.png')" img-top>     
+                </b-card-img>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione inventore velit ipsam dignissimos, quis facere ipsa minus accusantium nemo, sint illum unde corrupti! Laboriosam, repudiandae ex odit facilis possimus corporis.</p>
                 <!-- VOY A PONER UN BOTON -->
                 <b-button variant="danger" href="#">QUE LINDA FOTO</b-button>
-                </b-card>
+                <br><br>
             </b-col>
 
             <b-col cols="12" md="4">
                 <!-- SE PONE B-CARD PARA INDICAR QUE ES UNA TARJETA COME LA IMG DE LA PAG DE BOOTSTRAP..SOLO COPIE LO QUE ESTA AQUI DE LA PAG. DE BOOTSTRAP -->
-                <b-card img-src="https://picsum.photos/600/300/?image=25" img-top
-                >                
+                <b-card-img :src="require('../assets/2.png')" img-top>     
+                </b-card-img>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione inventore velit ipsam dignissimos, quis facere ipsa minus accusantium nemo, sint illum unde corrupti! Laboriosam, repudiandae ex odit facilis possimus corporis.</p>
                 <!-- VOY A PONER UN BOTON -->
                 <b-button variant="danger" href="#">QUE LINDA FOTO</b-button>
-                </b-card>
+                <br><br>
             </b-col>
 
             <b-col cols="12" md="4">
                 <!-- SE PONE B-CARD PARA INDICAR QUE ES UNA TARJETA COME LA IMG DE LA PAG DE BOOTSTRAP..SOLO COPIE LO QUE ESTA AQUI DE LA PAG. DE BOOTSTRAP -->
-                <b-card img-src="https://picsum.photos/600/300/?image=25" img-top
-                >                
+                <b-card-img :src="require('../assets/3.png')" img-top>     
+                </b-card-img>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione inventore velit ipsam dignissimos, quis facere ipsa minus accusantium nemo, sint illum unde corrupti! Laboriosam, repudiandae ex odit facilis possimus corporis.</p>
                 <!-- VOY A PONER UN BOTON -->
                 <b-button variant="danger" href="#">QUE LINDA FOTO</b-button>
-                </b-card>
+                <br><br>
             </b-col>
         </b-row>
-
 
 
 
