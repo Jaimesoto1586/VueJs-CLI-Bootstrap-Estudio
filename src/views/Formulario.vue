cad82423baa2a6c0c2636498ca7b57850d89cc12
Jaime <sotojaimeantonio@gmail.com>
2020-08-25 00:51:04 -0400

    Commit de arreglo template formulario

--- src/views/Formulario.vue
+++ src/views/Formulario.vue
@@ -34,7 +34,7 @@
         </b-form>
     
         <b-card class="mt-3" header="Form Data Result">
-        <pre class="m-0">{{ form }}</pre>
+         <pre class="m-0">{{ form }}</pre>
         </b-card>
         
       </form>
