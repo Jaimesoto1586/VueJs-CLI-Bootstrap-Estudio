e4811dd7f9629756c36a4f687610552f08049160
Jaime <sotojaimeantonio@gmail.com>
2020-08-27 18:01:52 -0400

    Commit del formulario, ver el data form: para sacar los objetos y hacer un alert de bienvenida

--- src/views/Formulario.vue
+++ src/views/Formulario.vue
@@ -3,31 +3,32 @@
       <h1 class="text-center mt-5 display-4">Formulario</h1>
       <form class="mt-5">
 
-          <!--NOMBRE  -->
-        <b-form-group id="input-group-2" label="Nombre:" label-for="input-2">
-            <b-form-input id="input-2" v-model="form.name" required placeholder="Nombre" :state="comprobarname"></b-form-input>
-        </b-form-group>
-
-        <!-- CORREO -->
+               <!-- CORREO  @submit="onSubmit" ENVIA EL FORMULARIO  @reset="onReset" LIMPIA EL FORMULARIO -->
         <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-            <b-form-group id="input-group-1"  label="Correo:" label-for="input-1" description="Tu correo es Privado; Nuca lo compartiremos con nadie">
-              <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Correo" :state="comprobaremail"></b-form-input>
-            </b-form-group>
+
+              <b-form-group id="input-group-1" label="Correo:" label-for="input-1" description="Tu correo es Privado; Nuca lo compartiremos con nadie">
+                <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Correo" :state="comprobaremail"></b-form-input>
+              </b-form-group>
+
+               <!--NOMBRE  -->
+              <b-form-group id="input-group-2" label="Nombre:" label-for="input-2">
+                  <b-form-input id="input-2" v-model="form.name"  required placeholder="Nombre" :state="comprobarname"></b-form-input>
+              </b-form-group>
+
+               <!-- SELECCIONA TU SECCION -->
+              <b-form-group id="input-group-3" label="Secciones:" label-for="input-3">
+                  <b-form-select id="input-3" v-model="form.secciones" :options="secciones" required></b-form-select>
+              </b-form-group>
 
 
-          <!-- SELECCIONA TU SECCION -->
-          <b-form-group id="input-group-3" label="Secciones:" label-for="input-3">
-              <b-form-select id="input-3" v-model="form.secciones" :options="secciones" required></b-form-select>
-          </b-form-group>
+               <!-- AREA DE TEXTO DE CONSULTA -->
+              <p>Consulta:</p>
+              <b-textarea v-model="form.textarea" required placeholder="Escribir" :state="comprobartextarea"></b-textarea>
 
 
-          <!-- AREA DE TEXTO DE CONSULTA -->
-          <p>Consulta:</p>
-          <b-textarea v-model="form.textarea" placeholder="Escribir" :state="comprobartextarea"></b-textarea>
+               <!-- BOTONES -->
+              <b-button class="mt-4" type="submit" variant="success">Enviar</b-button> <b-button class="mt-4" type="reset" variant="info">Limpiar</b-button>
 
-
-          <!-- BOTONES -->
-         <b-button class="mt-4" type="submit" variant="success">Enviar</b-button> <b-button class="mt-4" type="reset" variant="info">Limpiar</b-button>
         </b-form>
 
 
@@ -58,13 +59,10 @@
         show: true
       }
     },
+
     methods: {
       onSubmit(evt) {
         evt.preventDefault()
-        this.formHasErrors = this.form.email === '' || this.form.name === '' || this.form.secciones === null || this.form.textarea === ''
-        if(this.formHasErrors) {
-          return alert('!! Todos los campos son obligatorios ¡¡')
-        }
         alert(JSON.stringify(this.form))
       },
       onReset(evt) {
@@ -81,6 +79,7 @@
         })
       },
     },
+    
     computed: {
       comprobaremail(){
         return this.form.email.length > 0 ? true : false
